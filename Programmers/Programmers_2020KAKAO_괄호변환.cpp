32750737829737e273ab87f83d67b2343fe4b01a
stuart-park <hnn02148@gmail.com>
2021-10-03 21:01:49 +0900

    Update Programmers

--- Programmers/Programmers_2020KAKAO_괄호변환.cpp
+++ Programmers/Programmers_2020KAKAO_괄호변환.cpp
@@ -1,56 +1,56 @@
-//#include <string>
-//#include <vector>
-//#include <stack>
-//
-//using namespace std;
-//
-//string makeBalance(string p) {
-//    if (p.size() == 0)
-//        return "";
-//
-//    string u, v;
-//    int num1 = 0, num2 = 0;
-//    for (int i = 0; i < p.size(); i++) {
-//        if (num1 != 0 && num2 != 0 && num1 == num2) {
-//            v = p.substr(i, p.size() - i);
-//            break;
-//        }
-//        if (p[i] == '(') 
-//            num1++;
-//        else if (p[i] == ')') 
-//            num2++;
-//        u += p[i];
-//    }
-//
-//    stack<int> s;
-//    s.push(u[0]);
-//    for (int i = 1; i < u.size(); i++) {
-//        if (u[i] == '(')
-//            s.push(u[i]);
-//        else if (u[i] == ')') {
-//            if (s.top() == '(')
-//                s.pop();
-//            else
-//                s.push(u[i]);
-//        }
-//    }
-//
-//    if (s.size() == 0)
-//        return u + makeBalance(v);
-//
-//    else {
-//        string temp = "(" + makeBalance(v) + ")";
-//        string temp2 = "";
-//        for (int i = 1; i < u.size() - 1; i++) {
-//            char c = u[i] == ')' ? '(' : ')';
-//            temp2 += c;
-//        }
-//        temp += temp2;
-//        return temp;
-//    }
-//}
-//
-//string solution(string p) {
-//    string answer = makeBalance(p);
-//    return answer;
-//}
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+string makeBalance(string p) {
+    if (p.size() == 0)
+        return "";
+
+    string u, v;
+    int num1 = 0, num2 = 0;
+    for (int i = 0; i < p.size(); i++) {
+        if (num1 != 0 && num2 != 0 && num1 == num2) {
+            v = p.substr(i, p.size() - i);
+            break;
+        }
+        if (p[i] == '(') 
+            num1++;
+        else if (p[i] == ')') 
+            num2++;
+        u += p[i];
+    }
+
+    stack<int> s;
+    s.push(u[0]);
+    for (int i = 1; i < u.size(); i++) {
+        if (u[i] == '(')
+            s.push(u[i]);
+        else if (u[i] == ')') {
+            if (s.top() == '(')
+                s.pop();
+            else
+                s.push(u[i]);
+        }
+    }
+
+    if (s.size() == 0)
+        return u + makeBalance(v);
+
+    else {
+        string temp = "(" + makeBalance(v) + ")";
+        string temp2 = "";
+        for (int i = 1; i < u.size() - 1; i++) {
+            char c = u[i] == ')' ? '(' : ')';
+            temp2 += c;
+        }
+        temp += temp2;
+        return temp;
+    }
+}
+
+string solution(string p) {
+    string answer = makeBalance(p);
+    return answer;
+}
--- Programmers/Programmers_2020KAKAO_문자열압축.cpp
+++ Programmers/Programmers_2020KAKAO_문자열압축.cpp
@@ -1,41 +1,41 @@
-//#include <string>
-//#include <vector>
-//#include <iostream>
-//
-//int a = 1000000;
-//using namespace std;
-//
-//void startZip(string s, int size) {
-//    string ans = "";
-//
-//    int cnt = 1;
-//    string start = s.substr(0, size);
-//    for (int i = size; i < s.size(); i+=size) {
-//        string subString = s.substr(i, size);
-//        if (start != subString) {
-//            if (cnt == 1)
-//                ans = ans + start;
-//            else
-//                ans = ans + to_string(cnt) + start;
-//            cnt = 1;
-//            start = subString;
-//        }
-//        else
-//            cnt++;
-//    }
-//    if (cnt == 1)
-//        ans = ans + start;
-//    else
-//        ans = ans + to_string(cnt) + start;
-//
-//    if (ans.size() < a)
-//        a = ans.size();
-//}
-//
-//int solution(string s) {
-//    for (int i = 1; i <= s.size(); i++) 
-//        startZip(s, i);
-//
-//    int answer = a;
-//    return answer;
-//}
+#include <string>
+#include <vector>
+#include <iostream>
+
+int a = 1000000;
+using namespace std;
+
+void startZip(string s, int size) {
+    string ans = "";
+
+    int cnt = 1;
+    string start = s.substr(0, size);
+    for (int i = size; i < s.size(); i+=size) {
+        string subString = s.substr(i, size);
+        if (start != subString) {
+            if (cnt == 1)
+                ans = ans + start;
+            else
+                ans = ans + to_string(cnt) + start;
+            cnt = 1;
+            start = subString;
+        }
+        else
+            cnt++;
+    }
+    if (cnt == 1)
+        ans = ans + start;
+    else
+        ans = ans + to_string(cnt) + start;
+
+    if (ans.size() < a)
+        a = ans.size();
+}
+
+int solution(string s) {
+    for (int i = 1; i <= s.size(); i++) 
+        startZip(s, i);
+
+    int answer = a;
+    return answer;
+}
